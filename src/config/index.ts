35a1d5e10609172ac8d64668efc8ef42248bd307
Light Yagami <its_lightyagami@icloud.com>
2022-03-21 22:57:20 +0500

    Add guilds perm

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const host = 'https://api.animu.io/api/v1';
 const discordRedirectURI =
-  'https://discord.com/api/oauth2/authorize?client_id=585914522225868815&redirect_uri=https%3A%2F%2Fbeta.animu.io%2Flogin&response_type=code&scope=identify';
+  'https://discord.com/api/oauth2/authorize?client_id=585914522225868815&redirect_uri=https%3A%2F%2Fbeta.animu.io%2Flogin&response_type=code&scope=identify%20guilds';
 
 export { host, discordRedirectURI };
